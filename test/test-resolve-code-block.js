f9b39b8edad6032519e5694b84b83b771843e277
Ivan Demidov <Scrum@list.ru>
2020-03-23 10:53:48 +0300

    test: fails to remove fence if tag is indented, #1

---
+++ test/test-resolve-code-block.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const test = require('ava')
+const { readFileSync } = require('fs')
+const path = require('path')
+const resolveCodeBlock = require('../lib/resolve-code-block')
+const fixtures = path.join(__dirname, 'fixtures')
+
+test('markdown code', (t) => {
+  compare(t, 'code-block')
+})
+
+test('markdown code with indent', (t) => {
+  compare(t, 'code-block-with-indent')
+})
+
+function compare (t, name) {
+  const html = readFileSync(path.join(fixtures, `${name}.html`), 'utf8')
+  const expected = readFileSync(path.join(fixtures, `${name}.expected.html`), 'utf8')
+
+  t.deepEqual(resolveCodeBlock(html), expected)
+}
